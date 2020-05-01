12b68aadcb0134d6910b09ff8dfbeac47d3a556a
razalberto <raz_alberto@yahoo.com>
2020-05-01 19:44:04 +0300

    [HRA-2] GUI for registration created

--- src/main/java/registration/Main.java
+++ src/main/java/registration/Main.java
@@ -1,4 +1,4 @@
-package register;
+package registration;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,18 +6,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../resources/register.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
+        primaryStage.setTitle("Hotel Registration Application");
+        primaryStage.setScene(new Scene(root, 500, 450));
+        primaryStage.show();
     }
 }
---
+++ src/main/java/registration/controllers/RegistrationController.java
@@ -0,0 +1,36 @@
+package registration.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+
+
+public class RegistrationController {
+
+    @FXML
+    private Text registrationMessage;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField adressField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField phoneNumberField;
+    @FXML
+    private ChoiceBox role;
+
+    @FXML
+    public void initialize() {
+        role.getItems().addAll("Customer", "Hotel Manager");
+    }
+
+
+}
